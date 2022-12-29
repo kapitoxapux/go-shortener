ddb59be03de240e5066519f8a078cfbd57719bd7
Anton <kapitoxapux@gmail.com>
2022-12-29 21:43:06 +0300

    shorter generator update

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -1,9 +1,14 @@
 package storage
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
+	// "crypto/sha256"
+	// "encoding/hex"
+	"math/rand"
 )
+
+const letterBytes = "_.-~1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+var short string = ""
 
 var paths = map[string]*Shorter{}
 
@@ -14,23 +19,35 @@ type Shorter struct {
 }
 
 func Shortener(url string) string {
-	plainText := []byte(url)
-	sha256Hash := sha256.Sum256(plainText)
+	// plainText := []byte(url)
+	// sha256Hash := sha256.Sum256(plainText)
+	// return hex.EncodeToString(sha256Hash[:])
 
-	return hex.EncodeToString(sha256Hash[:])
+	b := make([]byte, 7)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+
+	if paths[string(b)] != nil {
+		return ""
+	}
+	return string(b)
+
 }
 
 func SetShort(url string) *Shorter {
-
-	id := Shortener(url)
+	short = ""
+	for short == "" {
+		short = Shortener(url)
+	}
 
 	shorter := new(Shorter)
 
-	shorter.ID = id
-	shorter.ShortURL = "http://localhost:8080/" + id
+	shorter.ID = short
+	shorter.ShortURL = "http://localhost:8080/" + short
 	shorter.LongURL = url
 
-	paths[shorter.ID] = shorter
+	paths[short] = shorter
 
 	return shorter
 }
