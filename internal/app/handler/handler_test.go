32a583063ebf2a1274a720193e46b5886756d264
Anton <kapitoxapux@gmail.com>
2023-01-05 02:41:19 +0300

    test_check

--- internal/app/handler/handler_test.go
+++ internal/app/handler/handler_test.go
@@ -12,133 +12,53 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func testCustomAction() http.HandlerFunc {
-	return func(res http.ResponseWriter, req *http.Request) {
-		switch req.Method {
-		case "POST":
-			if req.URL.Path != "/" {
-				http.Error(res, "Wrong route!", http.StatusNotFound)
+func testCustomAction(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "POST":
+		if req.URL.Path != "/" {
+			http.Error(res, "Wrong route!", http.StatusNotFound)
 
-				return
-			}
-
-			b, err := io.ReadAll(req.Body)
-			if err != nil {
-				http.Error(res, err.Error(), http.StatusBadRequest)
-
-				return
-			}
-
-			res.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			res.WriteHeader(http.StatusCreated)
-
-			short := storage.SetShort(string(b))
-
-			res.Write([]byte(short.ShortURL))
-
-		case "GET":
-			part := req.URL.Path
-			formated := strings.Replace(part, "/", "", -1)
-
-			sh := storage.GetShort(formated)
-			if sh == "" {
-				http.Error(res, "Url not founded!", http.StatusBadRequest)
-
-				return
-			}
-
-			res.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			res.Header().Set("Location", storage.GetFullURL(formated))
-			res.WriteHeader(http.StatusTemporaryRedirect)
-
-		default:
-			if req.Method != http.MethodGet {
-				http.Error(res, "Only GET and POST requests are allowed!", http.StatusNotFound)
-
-				return
-			}
-
+			return
 		}
-	}
-}
 
-func Test_getFullUrl(t *testing.T) {
-	tests := []struct {
-		name    string
-		link    string
-		shorter *storage.Shorter
-		equel   bool
-	}{
-		{
-			name:    "unique check",
-			link:    "http://localhost:8080/some_text_to_test_2",
-			shorter: storage.SetShort("http://localhost:8080/some_text_to_test_2"),
-			equel:   false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := storage.GetFullURL(tt.link); (got != tt.shorter.ShortURL) == tt.equel {
-				t.Errorf("getFullUrl() = %v, want %v", got, tt.equel)
-			}
-		})
-	}
-}
+		b, err := io.ReadAll(req.Body)
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusBadRequest)
 
-func Test_getShort(t *testing.T) {
-	tests := []struct {
-		name    string
-		link    string
-		shorter *storage.Shorter
-		equel   bool
-	}{
-		{
-			name:    "unique check",
-			link:    "http://localhost:8080/some_text_to_test_1",
-			shorter: storage.SetShort("http://localhost:8080/some_text_to_test_1"),
-			equel:   false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := storage.GetShort(tt.link); (got != tt.shorter.ShortURL) == tt.equel {
-				t.Errorf("getShort() = %v, want %v", got, tt.equel)
-			}
-		})
-	}
-}
+			return
+		}
 
-func Test_setShort(t *testing.T) {
+		res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		res.WriteHeader(http.StatusCreated)
 
-	testNegative := storage.SetShort("http://localhost:8080/some_text_to_test_1")
-	testPositive := storage.SetShort("http://localhost:8080/some_text_to_test_2")
+		short := storage.SetShort(string(b))
 
-	tests := []struct {
-		name    string
-		link    string
-		want    *storage.Shorter
-		wantErr bool
-	}{
-		{
-			name:    "new Shorter",
-			link:    "http://localhost:8080/some_text_to_test_2",
-			want:    testPositive,
-			wantErr: false,
-		},
-		{
-			name:    "catch error",
-			link:    "http://localhost:8080/some_text_to_test_1",
-			want:    testNegative,
-			wantErr: true,
-		},
+		res.Write([]byte(short.ShortURL))
+
+	case "GET":
+		part := req.URL.Path
+		formated := strings.Replace(part, "/", "", -1)
+
+		sh := storage.GetShort(formated)
+		if sh == "" {
+			http.Error(res, "Url not founded!", http.StatusBadRequest)
+
+			return
+		}
+
+		res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		res.Header().Set("Location", storage.GetFullURL(formated))
+		res.WriteHeader(http.StatusTemporaryRedirect)
+
+	default:
+		if req.Method != http.MethodGet {
+			http.Error(res, "Only GET and POST requests are allowed!", http.StatusNotFound)
+
+			return
+		}
+
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := testPositive; (got.ID != tt.want.ID) != tt.wantErr {
-				t.Errorf("setShort() = %v, want %v", got, tt.want.ID)
-			}
-		})
-	}
+
 }
 
 func TestEndpoints_Handle(t *testing.T) {
@@ -222,7 +142,7 @@ func TestEndpoints_Handle(t *testing.T) {
 
 			request := httptest.NewRequest(tt.method, tt.pattern, nil)
 			w := httptest.NewRecorder()
-			h := http.HandlerFunc(testCustomAction())
+			h := http.HandlerFunc(testCustomAction)
 			h(w, request)
 			result := w.Result()
 
---
+++ internal/app/storage/storage_test.go
@@ -0,0 +1,84 @@
+package storage
+
+import (
+	"testing"
+)
+
+func Test_getFullUrl(t *testing.T) {
+	tests := []struct {
+		name    string
+		link    string
+		shorter *Shorter
+		equel   bool
+	}{
+		{
+			name:    "unique check",
+			link:    "http://localhost:8080/some_text_to_test_2",
+			shorter: SetShort("http://localhost:8080/some_text_to_test_2"),
+			equel:   false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetFullURL(tt.link); (got != tt.shorter.ShortURL) == tt.equel {
+				t.Errorf("getFullUrl() = %v, want %v", got, tt.equel)
+			}
+		})
+	}
+}
+
+func Test_getShort(t *testing.T) {
+	tests := []struct {
+		name    string
+		link    string
+		shorter *Shorter
+		equel   bool
+	}{
+		{
+			name:    "unique check",
+			link:    "http://localhost:8080/some_text_to_test_1",
+			shorter: SetShort("http://localhost:8080/some_text_to_test_1"),
+			equel:   false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetShort(tt.link); (got != tt.shorter.ShortURL) == tt.equel {
+				t.Errorf("getShort() = %v, want %v", got, tt.equel)
+			}
+		})
+	}
+}
+
+func Test_setShort(t *testing.T) {
+
+	testNegative := SetShort("http://localhost:8080/some_text_to_test_1")
+	testPositive := SetShort("http://localhost:8080/some_text_to_test_2")
+
+	tests := []struct {
+		name    string
+		link    string
+		want    *Shorter
+		wantErr bool
+	}{
+		{
+			name:    "new Shorter",
+			link:    "http://localhost:8080/some_text_to_test_2",
+			want:    testPositive,
+			wantErr: false,
+		},
+		{
+			name:    "catch error",
+			link:    "http://localhost:8080/some_text_to_test_1",
+			want:    testNegative,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := testPositive; (got.ID != tt.want.ID) != tt.wantErr {
+				t.Errorf("setShort() = %v, want %v", got, tt.want.ID)
+			}
+		})
+	}
+}
