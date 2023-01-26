20281970953fa081bec90545f8a3d5172dee9f02
Anton <kapitoxapux@gmail.com>
2023-01-27 01:52:22 +0300

    pr fix

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -14,21 +14,29 @@ func main() {
 
 	addr := flag.String("a", "localhost:8080", "SERVER_ADDRESS")
 	base := flag.String("b", "http://localhost:8080", "BASE_URL")
-	path := flag.String("f", config.GetStoragePath(), "FILE_STORAGE_PATH")
+	path := flag.String("f", "json.txt", "FILE_STORAGE_PATH")
 
 	flag.Parse()
 
 	serverAdress := os.Getenv("SERVER_ADDRESS")
 	if serverAdress == "" {
+		os.Setenv("SERVER_ADDRESS", *addr)
 		serverAdress = *addr
 	}
 
 	baseURL := os.Getenv("BASE_URL")
 	if baseURL == "" {
+		os.Setenv("BASE_URL", *base)
 		baseURL = *base
 	}
 
-	env := config.SetEnvConf(serverAdress, baseURL, *path)
+	storagePath := os.Getenv("FILE_STORAGE_PATH")
+	if storagePath == "" {
+		os.Setenv("FILE_STORAGE_PATH", *path)
+		storagePath = *path
+	}
+
+	env := config.SetEnvConf(serverAdress, baseURL, storagePath)
 
 	server := &http.Server{
 		Addr:    env.Address,
