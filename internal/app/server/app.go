c8120ad7d6eeaff726b53601fb8edca0948c7bb0
Anton <kapitoxapux@gmail.com>
2023-03-01 00:39:44 +0300

    pr check

--- internal/app/server/app.go
+++ internal/app/server/app.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/go-chi/chi"
@@ -97,7 +98,7 @@ func (a *App) Run() error {
 	}()
 
 	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt, os.Interrupt)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
 	<-quit
 	ctx, shutdown := context.WithTimeout(context.Background(), 5*time.Second)
 	defer shutdown()
